5aeb2222c55db85fbe46df0482a1d73a6f024c9d
2bad2furious <22004964+2bad2furious@users.noreply.github.com>
2024-06-07 15:32:29 +0200

    ✨ Stop last playing sound when playing new sound

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const imageContainer = document.getElementById("imageContainer");
 
+  /** @var {Audio | undefined} */
+  let lastSound
+
   for (let i = 1; i <= 10; i++) {
     const img = document.createElement("img");
     img.src = `assets/img/borecek${i}.png`; // Assuming images are named image1.jpg, image2.jpg, etc.
@@ -11,7 +14,9 @@ document.addEventListener("DOMContentLoaded", () => {
     img.dataset.sound = `assets/audio/audio (mp3cut.net) (${i}).mp3`; // Assuming sounds are named sound1.mp3, sound2.mp3, etc.
 
     img.addEventListener("click", function () {
+      lastSound?.pause();
       const sound = new Audio(this.dataset.sound);
+      lastSound = sound;
       sound.play();
     });
 
